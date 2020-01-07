be3ccbaf33c60d980105185e002bf79f35eb6f38
Dan6erbond <moravrav@gmail.com>
2020-01-07 09:42:05 +0100

    animated launcher, ui updates

--- Chore App/app/src/main/java/ch/bbbaden/choreapp/LauncherActivity.kt
+++ Chore App/app/src/main/java/ch/bbbaden/choreapp/LauncherActivity.kt
@@ -1,19 +1,33 @@
 package ch.bbbaden.choreapp
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import ch.bbbaden.choreapp.child.ChildActivity
 import ch.bbbaden.choreapp.models.Child
 import ch.bbbaden.choreapp.models.Parent
 import ch.bbbaden.choreapp.parent.ParentActivity
 import ch.bbbaden.choreapp.signin.SignInActivity
+import kotlinx.android.synthetic.main.activity_launcher.*
+
 
 class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        val animator = ValueAnimator.ofInt(0, 56)
+        animator.addUpdateListener {
+            val params = imageView.layoutParams as ViewGroup.MarginLayoutParams
+            params.bottomMargin = it.animatedValue as Int
+            imageView.layoutParams = params
+            imageView.requestLayout()
+        }
+        animator.duration = 600
+        animator.start()
 
         UserManager.getUser {
             when (it) {
